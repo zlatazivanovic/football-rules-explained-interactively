59040fcecdbf5b9871c51abae203d381d7e0296e
zlatazivanovic <zlata_zivanovic@yahoo.com>
2026-03-30 09:07:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from rules import rules
 
-st.set_page_config(page_title="Football Rules Explained Interactively")
+FAQ_KEYS = ["offside", "red_card", "penalty_kick", "yellow_card", "handball_foul"]
 
+st.set_page_config(page_title="Football Rules Explained Interactively")
 st.title("Football Rules Explained Interactively")
 st.write("Select a football scenario below and learn the rules!")
 
@@ -17,5 +18,5 @@ if scenario:
     st.write(rules[scenario]["answer"])
 
 st.markdown("## Frequently Asked Questions")
-for key in rules:
+for key in FAQ_KEYS:
     st.markdown(f"**{rules[key]['question']}**  \n{rules[key]['answer']}")
